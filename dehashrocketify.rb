a4ed99878ea9bb1c700db5fdda31ae84b1430355
thoran <code@thoran.com>
2023-03-29 20:16:15 +1100

    + dehashrocketify-0.8.0

---
+++ dehashrocketify.rb
@@ -0,0 +1,15 @@
+class Dehashrocketify < Formula
+
+  desc "Hashrockets be gone!"
+  homepage 'https://github.com/thoran/dehashrocketify'
+  version '0.8.0'
+
+  url 'https://github.com/thoran/dehashrocketify.git'
+
+  depends_on 'ruby'
+
+  def install
+    bin.install 'bin/dehashrocketify'
+  end
+
+end
