22613b71017475224806bba02b18aab7eb6dca5a
thoran <code@thoran.com>
2023-03-30 21:06:50 +1100

    ~ dehashrocketify: 0.9.0 --> 0.9.1

--- dehashrocketify.rb
+++ dehashrocketify.rb
@@ -2,7 +2,7 @@ class Dehashrocketify < Formula
 
   desc "Hashrockets be gone!"
   homepage 'https://github.com/thoran/dehashrocketify'
-  version '0.9.0'
+  version '0.9.1'
 
   url 'https://github.com/thoran/dehashrocketify.git'
 
