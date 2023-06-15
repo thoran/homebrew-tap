5e789f9e4f6159b9cb98e33fade4ace00ae71dbb
thoran <code@thoran.com>
2023-06-15 22:51:22 +1000

    + git-messages-0.0.0

---
+++ git-messages.rb
@@ -0,0 +1,15 @@
+class GitMessages < Formula
+
+  desc "Show the list of conventional commit types."
+  homepage 'https://github.com/thoran/git-messages'
+  version '0.0.0'
+
+  url 'https://github.com/thoran/git-messages.git'
+
+  depends_on 'ruby'
+
+  def install
+    bin.install 'bin/git-messages'
+  end
+
+end
