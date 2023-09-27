02a2cb7699bb96173c3fb05fc1237a306a7dfb2d
thoran <code@thoran.com>
2023-09-27 14:06:35 +1000

    ~ cases: 0.0.1 --> 0.1.0

--- cases.rb
+++ cases.rb
@@ -2,7 +2,7 @@ class Cases < Formula
 
   desc "Show a list of the different cases used when coding."
   homepage 'https://github.com/thoran/cases'
-  version '0.0.1'
+  version '0.1.0'
 
   url 'https://github.com/thoran/cases.git'
 
