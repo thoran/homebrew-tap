23c51f84f5255a82838b511a2c4e091ac64b87fd
thoran <code@thoran.com>
2026-08-09 01:50:17 +1000

    ~ skyhooks: 0.3.1 --> 0.4.0

--- skyhooks.rb
+++ skyhooks.rb
@@ -2,7 +2,7 @@ class Skyhooks < Formula
 
   desc "Automatically swap references from FactoryBot's create to build in tests to speed up test runs by using in-memory objects (in the sky) rather than hitting the database (down below)."
   homepage 'https://github.com/thoran/skyhooks'
-  version '0.3.1'
+  version '0.4.0'
 
   url 'https://github.com/thoran/skyhooks.git'
 
