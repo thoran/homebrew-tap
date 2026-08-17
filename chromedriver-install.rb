ae8efb9049ea67ca90f48446ff04aed6bf57f7c4
thoran <code@thoran.com>
2026-08-18 01:00:19 +1000

    ~ chromedriver-install: 0.6.0 --> 0.6.1

--- chromedriver-install.rb
+++ chromedriver-install.rb
@@ -1,8 +1,7 @@
 class ChromedriverInstall < Formula
-
   desc "Automatic selection and installation of the right chromedriver for your version of Chrome."
   homepage 'https://github.com/thoran/chromedriver-install'
-  version '0.6.0'
+  version '0.6.1'
 
   url 'https://github.com/thoran/chromedriver-install.git'
 
@@ -46,5 +45,4 @@ class ChromedriverInstall < Formula
       RUBYLIB: libexec/'lib',
     )
   end
-
 end
