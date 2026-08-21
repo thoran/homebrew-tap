d1e8f88212d363dd282db05de1fd04bf3f8adc24
thoran <code@thoran.com>
2026-08-21 10:01:49 +1000

    ~ weblink: 0.10.0 --> 0.11.1

--- weblink.rb
+++ weblink.rb
@@ -1,6 +1,6 @@
 class Weblink < Formula
   desc "A CLI program for generating and converting weblink or web shortcut files."
-  version '0.10.0'
+  version '0.11.1'
 
   homepage 'https://github.com/thoran/weblink'
   url 'https://github.com/thoran/weblink.git'
