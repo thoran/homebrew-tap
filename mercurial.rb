7b5b32562831b6718b75420d55247ee76f42d233
thoran <code@thoran.com>
2026-08-21 13:26:37 +1000

    ~ mercurial: 0.12.0 --> 0.12.1

--- mercurial.rb
+++ mercurial.rb
@@ -1,7 +1,7 @@
 class Mercurial < Formula
   desc "Wireguard server switcher."
   homepage 'https://github.com/thoran/mercurial'
-  version '0.12.0'
+  version '0.12.1'
 
   url 'https://github.com/thoran/mercurial.git'
 
