008bd1a9708718259b8b71771218c29724dd4ede
thoran <code@thoran.com>
2026-08-21 13:26:37 +1000

    ~ startor: 0.9.0 --> 0.9.1

--- startor.rb
+++ startor.rb
@@ -1,7 +1,7 @@
 class Startor < Formula
   desc "Easily install, start, and stop tor."
   homepage 'https://github.com/thoran/startor'
-  version '0.9.0'
+  version '0.9.1'
 
   url 'https://github.com/thoran/startor.git'
 
