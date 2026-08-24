dc22a9996609a8a21f6744196ee7d3d54546205c
thoran <code@thoran.com>
2026-08-24 17:33:07 +1000

    ~ chromedriver-install: 0.7.2 --> 0.7.3

--- chromedriver-install.rb
+++ chromedriver-install.rb
@@ -1,7 +1,7 @@
 class ChromedriverInstall < Formula
   desc "Automatic selection and installation of the right chromedriver for your version of Chrome."
   homepage 'https://github.com/thoran/chromedriver-install'
-  version '0.7.2'
+  version '0.7.3'
   license 'MIT'
 
   url 'https://github.com/thoran/chromedriver-install.git'
