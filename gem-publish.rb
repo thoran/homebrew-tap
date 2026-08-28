d9971717625c7b2545abc4a9114f23adf082224a
thoran <code@thoran.com>
2026-08-28 21:55:26 +1000

    ~ gem-publish: 0.2.1 --> 0.3.0

--- gem-publish.rb
+++ gem-publish.rb
@@ -1,7 +1,7 @@
 class GemPublish < Formula
   desc "A simple shell script to build, push, and clean up a gem in one step."
   homepage 'https://github.com/thoran/gem-publish'
-  version '0.2.1'
+  version '0.3.0'
   license 'MIT'
 
   url 'https://github.com/thoran/gem-publish.git'
